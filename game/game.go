6b5731cfe39932683761dbd6a321d0414891df85
Dan Krishnan <dan@wearecodenation.com>
2019-12-23 00:19:54 +0000

    added error handling

--- game/game.go
+++ game/game.go
@@ -8,7 +8,7 @@ import (
 )
 
 //Logic for the Rock Paper Scissors game, generates a random choice for the opponents then determines a winner
-func Logic(choice string) {
+func Logic(choice string) int {
 	// generate a random number between 0 and 2
 	rand.Seed(time.Now().UnixNano())
 	n := rand.Intn(3)
@@ -25,34 +25,39 @@ func Logic(choice string) {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	WHAT?! That cannot be? You must have cheated")
-
+			return 1
 		} else if opponentChoice == "paper" {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	HAHA! You'll never beat me.")
+			return 0
 		}
 	} else if c == "gopher" {
 		if opponentChoice == "rock" {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	HAHA! You'll never beat me.")
+			return 0
 		} else if opponentChoice == "paper" {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	WHAT?! That cannot be? You must have cheated")
+			return 1
 		}
 	} else if c == "paper" {
 		if opponentChoice == "rock" {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	WHAT?! That cannot be? You must have cheated")
+			return 1
 		} else if opponentChoice == "gopher" {
 			fmt.Println("	You picked " + c)
 			fmt.Println("	I picked " + opponentChoice)
 			fmt.Println("	HAHA! You'll never beat me.")
+			return 0
 		}
 	} else {
 		fmt.Println("	Hahah, you didn't choose a proper weapon. Are you scared?")
 	}
-
+	return 2
 }
